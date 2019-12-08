02f42998cbc40b803beef75742e3de108987ffb5
mikhail <skorikov.m@list.ru>
2019-12-08 23:03:24 +0300

    Settings

--- Python/alien_invasion.py
+++ Python/alien_invasion.py
@@ -1,12 +1,19 @@
 import sys
 import pygame
+from settings import Settings
+
 
 def run_game():
 	pygame.init()
-	pygame.display.set_mode((1200,800))
+	stn = Settings()
+	screen=pygame.display.set_mode((stn.width,stn.height))
+	pygame.display.set_caption('Alien Invasion')
+	bg_color = stn.bg_color
 	while True:
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				sys.exit()
+		screen.fill(bg_color)
 		pygame.display.flip()
+		
 run_game()
